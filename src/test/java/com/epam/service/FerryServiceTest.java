6ce590772f79c1844644f1db82c96763c4121468
vzapatylak <olga.zapotylok.98@mail.ru>
2019-01-25 11:54:37 +0300

    Add tests for service layer.

--- src/test/java/com/epam/service/FerryServiceTest.java
+++ src/test/java/com/epam/service/FerryServiceTest.java
@@ -1,6 +1,34 @@
 package com.epam.service;
 
 import com.epam.BaseTest;
+import com.epam.entity.TypeOfCar;
+import com.epam.entity.VehicleEntity;
+
+import java.util.HashSet;
+
+import static org.junit.Assert.assertEquals;
 
 public class FerryServiceTest extends BaseTest {
+
+    public void testPayloadOverflow() {
+        ferry.setPayload(660);
+        HashSet<VehicleEntity> vehicles = new HashSet<>();
+        vehicles.add(new VehicleEntity(300, 100, TypeOfCar.CAR));
+        vehicles.add(new VehicleEntity(300, 100, TypeOfCar.CAR));
+        vehicles.add(new VehicleEntity(300, 100, TypeOfCar.CAR));
+
+        ferry.setVehicles(vehicles);
+        assertEquals(ferry.getVehicles().size(), 2);
+    }
+
+    public void testAreaOverflow() {
+        ferry.setAreaOfPlatform(220);
+        HashSet<VehicleEntity> vehicles = new HashSet<>();
+        vehicles.add(new VehicleEntity(300, 100, TypeOfCar.CAR));
+        vehicles.add(new VehicleEntity(300, 100, TypeOfCar.CAR));
+        vehicles.add(new VehicleEntity(300, 100, TypeOfCar.CAR));
+
+        ferry.setVehicles(vehicles);
+        assertEquals(ferry.getVehicles().size(), 2);
+    }
 }
---
+++ src/test/java/com/epam/service/VehicleServiceTest.java
@@ -0,0 +1,6 @@
+package com.epam.service;
+
+import com.epam.BaseTest;
+
+public class VehicleServiceTest extends BaseTest {
+}
