e33efcbc6ef31548d891134a0d56495ef604a474
vzapatylak <olga.zapotylok.98@mail.ru>
2019-01-25 11:58:46 +0300

    Change service classes.

--- src/test/java/com/epam/service/FerryServiceTest.java
+++ src/test/java/com/epam/service/FerryServiceTest.java
@@ -4,18 +4,17 @@ import com.epam.BaseTest;
 import com.epam.entity.TypeOfCar;
 import com.epam.entity.VehicleEntity;
 
-import java.util.HashSet;
-
 import static org.junit.Assert.assertEquals;
 
 public class FerryServiceTest extends BaseTest {
 
+    private FerryService ferryService;
+
     public void testPayloadOverflow() {
         ferry.setPayload(660);
-        HashSet<VehicleEntity> vehicles = new HashSet<>();
-        vehicles.add(new VehicleEntity(300, 100, TypeOfCar.CAR));
-        vehicles.add(new VehicleEntity(300, 100, TypeOfCar.CAR));
-        vehicles.add(new VehicleEntity(300, 100, TypeOfCar.CAR));
+        ferryService.addVehicle(new VehicleEntity(300, 100, TypeOfCar.CAR));
+        ferryService.addVehicle(new VehicleEntity(300, 100, TypeOfCar.CAR));
+        ferryService.addVehicle(new VehicleEntity(300, 100, TypeOfCar.CAR));
 
         ferry.setVehicles(vehicles);
         assertEquals(ferry.getVehicles().size(), 2);
@@ -23,10 +22,9 @@ public class FerryServiceTest extends BaseTest {
 
     public void testAreaOverflow() {
         ferry.setAreaOfPlatform(220);
-        HashSet<VehicleEntity> vehicles = new HashSet<>();
-        vehicles.add(new VehicleEntity(300, 100, TypeOfCar.CAR));
-        vehicles.add(new VehicleEntity(300, 100, TypeOfCar.CAR));
-        vehicles.add(new VehicleEntity(300, 100, TypeOfCar.CAR));
+        ferryService.addVehicle(new VehicleEntity(300, 100, TypeOfCar.CAR));
+        ferryService.addVehicle(new VehicleEntity(300, 100, TypeOfCar.CAR));
+        ferryService.addVehicle(new VehicleEntity(300, 100, TypeOfCar.CAR));
 
         ferry.setVehicles(vehicles);
         assertEquals(ferry.getVehicles().size(), 2);
