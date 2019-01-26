340c7e75c9f5f44b293bd989a49a16d2f2a66719
vzapatylak <olga.zapotylok.98@mail.ru>
2019-01-26 10:30:36 +0300

    Change BaseTest.

--- src/test/java/com/epam/BaseTest.java
+++ src/test/java/com/epam/BaseTest.java
@@ -3,27 +3,30 @@ package com.epam;
 import com.epam.entity.FerryEntity;
 import com.epam.entity.VehicleEntity;
 import com.epam.service.VehicleService;
+import com.epam.service.VehicleServiceImpl;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import java.util.Set;
+import java.net.URISyntaxException;
+import java.util.List;
 
 @RunWith(JUnit4.class)
 public class BaseTest {
-    protected FerryEntity ferry;
-    protected Set<VehicleEntity> vehicles;
-    protected VehicleService vehicleService;
+    protected static FerryEntity ferry;
+    protected static List<VehicleEntity> vehicles;
+    protected static VehicleService vehicleService;
 
     @BeforeClass
-    public void init() {
+    public static void init() throws URISyntaxException {
+        vehicleService = new VehicleServiceImpl();
         ferry = FerryEntity.getInstance();
         vehicles = vehicleService.getAll();
     }
 
     @AfterClass
-    public void close() {
+    public static void close() {
         ferry = null;
         vehicles = null;
     }
