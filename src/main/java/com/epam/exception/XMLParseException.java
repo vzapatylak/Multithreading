2081aabeabda19786dd7bc006cf8053ff9243af1
vzapatylak <olga.zapotylok.98@mail.ru>
2019-01-26 09:59:52 +0300

    Add .json files.

--- src/main/java/com/epam/exception/XMLParseException.java
+++
@@ -1,8 +0,0 @@
-package com.epam.exception;
-
-public class XMLParseException extends Exception {
-
-    public XMLParseException(String message) {
-        super("XML parse exception: " + message);
-    }
-}
--- src/main/java/com/epam/validator/XSDValidator.java
+++
@@ -1,9 +0,0 @@
-package com.epam.validator;
-
-public class XSDValidator implements Validator {
-
-    @Override
-    public boolean isValid() {
-        return false;
-    }
-}
